2324276f1a05a18d6d1b0e2803401ef9323ee7d5
[Aman Yadav] <[aman4864y@gmail.com]>
2024-04-09 02:01:27 +0530

    "Add Task, description for user input and Add Task button" 	modified:   app/globals.css 	modified:   app/page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,23 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <h1 className="bg-black text-white p-5 text-5xl font-bold text-center">
+    Aman's Todo list</h1>
+    <form>
+      <input type="text" className="text-2xl border-zinc-800 
+        border-2 m-5 px-4 py-2"
+        placeholder="Enter Task Here"
+      />
+      <input type="text" className="text-2xl border-zinc-800 
+        border-2 m-5 px-4 py-2"
+        placeholder="Enter Description Here"
+      />
+      <button className="bg-black text-white px-4 py-3 
+        text-2xl font-bold rounded m-5">Add Task
+      </button>
+    </form>
+    </>
   )
 }
 
